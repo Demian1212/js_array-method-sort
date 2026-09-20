39454bcf3f5e5739203f6a0864cc0e442e2c196d
Demian Pishchak <demanpisak191@gmail.com>
2026-09-20 14:11:25 +0300

    remove comments

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,27 +5,29 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    // Якщо порівняльну функцію не передано, використовуємо дефолтне порівняння як рядків
-    const compare = compareFunction || function (a, b) {
-      const aStr = String(a);
-      const bStr = String(b);
+    const compare =
+      compareFunction ||
+      function (a, b) {
+        const aStr = String(a);
+        const bStr = String(b);
 
-      if (aStr < bStr) {
-        return -1;
-      }
+        if (aStr < bStr) {
+          return -1;
+        }
 
-      if (aStr > bStr) {
-        return 1;
-      }
+        if (aStr > bStr) {
+          return 1;
+        }
 
-      return 0;
-    };
+        return 0;
+      };
 
     // Алгоритм сортування бульбашкою (Bubble Sort)
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
         if (compare(this[j], this[j + 1]) > 0) {
           const temp = this[j];
+
           this[j] = this[j + 1];
           this[j + 1] = temp;
         }
