b1ca825c55d9345fad4a31ae38583cc975da3e18
Demian Pishchak <demanpisak191@gmail.com>
2026-09-20 14:07:06 +0300

    solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,36 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    // Якщо порівняльну функцію не передано, використовуємо дефолтне порівняння як рядків
+    const compare = compareFunction || function (a, b) {
+      const aStr = String(a);
+      const bStr = String(b);
+
+      if (aStr < bStr) {
+        return -1;
+      }
+
+      if (aStr > bStr) {
+        return 1;
+      }
+
+      return 0;
+    };
+
+    // Алгоритм сортування бульбашкою (Bubble Sort)
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          const temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    // Обов'язково повертаємо той самий масив
+    return this;
   };
 }
 
